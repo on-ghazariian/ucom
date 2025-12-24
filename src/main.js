4c29fd1cbe8b6cf7de93c97140cea086b757c9e7
ghazarian009ait-crypto <ghazarian009ait@gmail.com>
2025-12-24 17:26:07 +0400

    add contacts div

--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ document.querySelector("#app").innerHTML = `
         <p id="anh">Անհատներ</p>
         <p id="bzn">Բիզնես</p>
       </div>
+      <div id="contacts">
+        <i class="fa-solid fa-phone"></i>
+        <p id="four">444</p>
+      </div>
     </div>      
   </div>
 `;
