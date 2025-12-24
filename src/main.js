f26c04d3f332ff49c9af7706c7660efb3f364f7b
ghazarian009ait-crypto <ghazarian009ait@gmail.com>
2025-12-24 17:02:57 +0400

    create category and add style

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <div id="header">
+    <div id="top-header">
+      <div id="category">
+        <p id="anh">Անհատներ</p>
+        <p id="bzn">Բիզնես</p>
+      </div>
+    </div>      
+  </div>
+`;
